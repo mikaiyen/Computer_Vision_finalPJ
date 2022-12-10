0838cfbe4cc595a5fbca4baa9e56c480e7d58e05
mikaiyen <mikai890820@gmail.com>
2022-12-11 01:44:30 +0800

    initial

---
+++ cvcaptcha.py
@@ -0,0 +1,112 @@
+import numpy as np # linear algebra
+import pandas as pd
+#%matplotlib inline 
+import matplotlib.pyplot as plt
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from keras import layers
+from keras.models import Model
+from keras.models import load_model
+from keras import callbacks
+import cv2
+import string
+
+#Init main values
+symbols = string.ascii_lowercase + "0123456789" # All symbols captcha can contain
+num_symbols = len(symbols)
+img_shape = (50, 200, 1)
+
+def create_model():
+    img = layers.Input(shape=img_shape) # Get image as an input and process it through some Convs
+    conv1 = layers.Conv2D(16, (3, 3), padding='same', activation='relu')(img)
+    mp1 = layers.MaxPooling2D(padding='same')(conv1)  # 100x25
+    conv2 = layers.Conv2D(32, (3, 3), padding='same', activation='relu')(mp1)
+    mp2 = layers.MaxPooling2D(padding='same')(conv2)  # 50x13
+    conv3 = layers.Conv2D(32, (3, 3), padding='same', activation='relu')(mp2)
+    bn = layers.BatchNormalization()(conv3)
+    mp3 = layers.MaxPooling2D(padding='same')(bn)  # 25x7
+    
+    # Get flattened vector and make 5 branches from it. Each branch will predict one letter
+    flat = layers.Flatten()(mp3)
+    outs = []
+    for _ in range(5):
+        dens1 = layers.Dense(64, activation='relu')(flat)
+        drop = layers.Dropout(0.5)(dens1)
+        res = layers.Dense(num_symbols, activation='sigmoid')(drop)
+
+        outs.append(res)
+    
+    # Compile model and return it
+    model = Model(img, outs)
+    model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=["accuracy"])
+    return model
+
+def modelexist():
+    if os.path.isfile(modelfilepath):
+        return True
+
+
+def preprocess_data():
+    n_samples = len(os.listdir('./input/captcha-version-2-images'))
+    X = np.zeros((n_samples, 50, 200, 1)) #1070*50*200
+    y = np.zeros((5, n_samples, num_symbols)) #5*1070*36
+
+    for i, pic in enumerate(os.listdir('./input/captcha-version-2-images')):
+        # Read image as grayscale
+        img = cv2.imread(os.path.join('./input/captcha-version-2-images', pic), cv2.IMREAD_GRAYSCALE)
+        pic_target = pic[:-4]
+        if len(pic_target) < 6:
+            # Scale and reshape image
+            img = img / 255.0
+            img = np.reshape(img, (50, 200, 1))
+            # Define targets and code them using OneHotEncoding
+            targs = np.zeros((5, num_symbols))
+            for j, l in enumerate(pic_target):
+                ind = symbols.find(l)
+                targs[j, ind] = 1
+            X[i] = img
+            y[:, i] = targs
+    
+    # Return final data
+    return X, y
+
+modelfilepath = "./captcha.h5"
+if os.path.isfile(modelfilepath):
+    model = load_model('./captcha.h5')
+    
+def build_model(bsize,eps):
+    X, y = preprocess_data()
+    X_train, y_train = X[:970], y[:, :970]
+    X_test, y_test = X[970:], y[:, 970:]
+    model=create_model();
+    #hist = model.fit(X_train, [y_train[0], y_train[1], y_train[2], y_train[3], y_train[4]], batch_size=32, epochs=30,verbose=1, validation_split=0.2)
+    model.fit(X_train, [y_train[0], y_train[1], y_train[2], y_train[3], y_train[4]], batch_size=bsize, epochs=eps,verbose=1, validation_split=0.2)
+    model.save('captcha.h5')
+    score= model.evaluate(X_test,[y_test[0], y_test[1], y_test[2], y_test[3], y_test[4]],verbose=1)
+    print('Test Loss and accuracy:', score)
+
+
+def predict(filepath):
+    img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    if img is not None:
+        img = img / 255.0
+    else:
+        print("Not detected");
+    res = np.array(model.predict(img[np.newaxis, :, :, np.newaxis]))
+    ans = np.reshape(res, (5, 36))
+    l_ind = []
+    probs = []
+    for a in ans:
+        l_ind.append(np.argmax(a))
+        #probs.append(np.max(a))
+
+    capt = ''
+    for l in l_ind:
+        capt += symbols[l]
+    return capt#, sum(probs) / 5
+
+# import matplotlib.pyplot as plt
+# print("Predicted Captcha =",predict('./input/capthaimages/a.png'))
+# img=cv2.imread('./input/capthaimages/a.png',cv2.IMREAD_GRAYSCALE)
+# imgplot=plt.imshow(img)
+# plt.show()
---
+++ main.py
@@ -0,0 +1,95 @@
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from tkinter import filedialog
+import cvcaptcha as cp
+from PIL import Image, ImageTk
+from tkinter import messagebox
+
+def choosedata():
+    global file_path, havefile
+    file_path = filedialog.askopenfilename()   # 選擇檔案後回傳檔案路徑與名稱
+    name_text.set(file_path)
+    
+
+def cvpredict():
+    global img, tk_img
+    if (name_text.get()!=""):
+        Predict_text.set('Prediction:'+cp.predict(file_path))
+        img = Image.open(file_path)        # 開啟圖片
+        tk_img = ImageTk.PhotoImage(img)    # 轉換為 tk 圖片物件
+        '''
+        largeimg = tk_img.tk.zoom(2, 2)  
+        label = tk.Label(root, image=largeimg)  # 在 Lable 中放入圖片
+        label.image = largeimg
+        '''
+        photolabel.config(image=tk_img)
+        photolabel.pack()
+    else:
+        messagebox.showerror('Error', 'No data selected!')
+    
+    
+def buildmodel():
+    if batch_size_button.get()!="":
+        bsize=int(batch_size_button.get())
+    else:
+        bsize=32
+        
+    if batch_size_button.get()!="":
+        eps=int(epoch_button.get())
+    else:
+        eps=30
+        
+    if (cp.modelexist):
+        result = messagebox.askyesno("model detected", "Detected exist model, do you want to rebuild new model?")
+        if(result==True):
+            cp.build_model(bsize, eps)
+    else:
+        cp.build_model(bsize, eps)
+    
+
+
+
+
+root = tk.Tk()
+root.title('109502561')
+root.geometry('800x500')
+root.configure(bg='#97CBFF')
+havefile=0
+#-------------------------------
+training_button = tk.Button(root, text='Training~（´◔​∀◔`)', command=buildmodel,fg="#2828FF",bg="#FFFFFB")
+training_button.place(x=300,y=430)
+
+
+
+predict_button = tk.Button(root, text='Predict~（´◔​∀◔`)', command=cvpredict,fg="#2828FF",bg="#FFFFFB")
+predict_button.place(x=600,y=430)
+#-------------------------------------------------
+batch_size_button= tk.Entry(root)
+batch_size_button.place(x=250, y=310)
+
+epoch_button= tk.Entry(root)
+epoch_button.place(x=250,y=350)
+
+batch_size_label= tk.Label(root, text= 'batch size: (init=32)', fg="#2828FF",bg="#97CBFF").place(x=125, y=310)
+
+epoch_label = tk.Label(root, text= "epoch: (init=30)", fg="#2828FF",bg="#97CBFF").place(x=125, y=350)
+
+Predict_text=tk.StringVar()
+Predict_text.set('Prediction:')
+Predict_label = tk.Label(root, textvariable= Predict_text, fg="#2828FF",bg="#97CBFF")
+Predict_label.place(x=450, y=310)
+
+photolabel = tk.Label(root)  
+#-------------------------------------------------
+flower_label = tk.Label(root, text = "`*-.,.-*`♩`*-.,.-*`♪`*-.,.-*`♫`*-.,.-*`♬`*-.,.-*`♩`*-.,.-*`♪`*-.,.-*`♫`*-.,.-*`*-.,.-*`♩`*-.,.-*`♪`*-.,.-*`", fg="#2828FF",bg="#97CBFF",font=1)
+flower_label.place(x=0, y=470)
+
+#-----------------------------------------------------
+choose_data = tk.Button(root,text='choose data',command=choosedata, fg="#2828FF",bg="#FFFFFB")
+choose_data.place(x=10,y=310)
+name_text = tk.StringVar()
+name_label = tk.Label(root, textvariable=name_text, fg="#2828FF", bg="#97CBFF")
+name_label.place(x=10, y=370)
+#ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+
+root.mainloop()
